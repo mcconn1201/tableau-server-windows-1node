dc51ee916c01886e2c210e7bea779025c091aa16
Madeleine Corneli <mcorneli@tableau.com>
2018-05-08 09:56:42 -0700

    new start script

--- config_script.ps1
+++ config_script.ps1
@@ -22,7 +22,6 @@ Param(
 ## 1. make secrets.json file
 
 cd C:/
-mkdir tabsetup
 
 $secrets = @{
     content_admin_user = "$ts_admin_un"
---
+++ invoke.ps1
@@ -0,0 +1,36 @@
+Param(
+    [string]$windows_un,
+    [string]$windows_pw,
+    [string]$ts_admin_un,
+    [string]$ts_admin_pw,
+    [string]$reg_first_name,
+    [string]$reg_last_name,
+    [string]$reg_email,
+    [string]$reg_company,
+    [string]$reg_title,
+    [string]$reg_department,
+    [string]$reg_industry,
+    [string]$reg_phone,
+    [string]$reg_city,
+    [string]$reg_state,
+    [string]$reg_zip,
+    [string]$reg_country,
+    [string]$license_key,
+    [boolean]$trial
+)
+
+## download config-script
+mkdir "C:/tabsetup/"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/maddyloo/tableau-server-windows-1node/master/config_script.ps1" -OutFile "C:/tabsetup/config-script.ps1"
+
+## enable permissions
+$password = ConvertTo-SecureString $windows_pw -AsPlainText -Force
+$path = $env:COMPUTERNAME + "\" + $windows_un
+$path
+$credential = New-Object System.Management.Automation.PSCredential($path, $password)
+Enable-PSRemoting -Force
+
+## call config script
+Invoke-Command -FilePath "c:/tabsetup/config-script.ps1" -Credential $credential -ComputerName $env:COMPUTERNAME -ArgumentList $ts_admin_un, $ts_admin_pw, $reg_first_name, $reg_last_name, $reg_email, $reg_company, $reg_title, $reg_department, $reg_industry, $reg_phone, $reg_city, $reg_state, $reg_zip, $reg_country, $license_key, $trial
+
+Disable-PSRemoting -Force
