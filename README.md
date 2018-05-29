3ce4d10db568c200c73bf4e8d0f3faca384ee62b
Madeleine Corneli <mcorneli@tableau.com>
2018-05-29 14:25:35 -0700

    clean up

--- README.md
+++ README.md
@@ -1,10 +1,6 @@
 https://docs.microsoft.com/en-us/azure/virtual-machines/windows/faq#what-are-the-username-requirements-when-creating-a-vm
 
-# Tableau Server on Windows - Single Node
-
-Tableau Server on Azure is browser and mobile-base visual analytics anyone can use.  Publish interactive dashboards with Tableau Desktop and share them throughout your organization. Embedded or as a stand-alone application, you can empower your business to find answers in minutes, not months.  By deploying Tableau Server on Azure with this quickstart you can take full advantage of the power and flexibility of Azure Cloud infrastructure.  
-
-Tableau helps tens of thousands of people see and understand their data by making it simple for the everyday data worker to perform ad-hoc visual analytics and data discovery as well as the ability to seamlessly build beautiful dashboards and reports. Tableau is designed to make connecting live to data of all types a simple process that doesn't require any coding or scripting. From cloud sources like Azure SQL Data Warehouse, to on-premise Hadoop clusters, to local spreadsheets, Tableau gives everyone the power to quickly start visually exploring data of any size to find new insights.    
+# Tableau Server on Windows - Single Node   
 
 <a href="https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2FAzure%2Fazure-quickstart-templates%2Fmaster%2F100-blank-template%2Fazuredeploy.json" target="_blank">
 <img src="https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/1-CONTRIBUTION-GUIDE/images/deploytoazure.png"/>
--- scripts/config_script.ps1
+++ scripts/config_script.ps1
@@ -20,7 +20,6 @@ Param(
 ## FILES
 
 ## 1. make secrets.json file
-
 cd C:/
 mkdir tabsetup
 
@@ -32,8 +31,6 @@ $secrets = @{
 $secrets | ConvertTo-Json -depth 10 | Out-File "C:/tabsetup/secrets.json" -Encoding ASCII
 
 ## 2. make registration.json
-
-# add AZURE QUICKSTART TO COMPANY TO TRACK
 $registration = @{
     first_name = "$reg_first_name"
     last_name = "$reg_last_name"
@@ -51,31 +48,31 @@ $registration = @{
 
 $registration | ConvertTo-Json -depth 10 | Out-File "C:/tabsetup/registration.json" -Encoding ASCII
 
-## 3. download python installer (refers to Tableau's github page)
+## 3. Download python installer (refers to Tableau's github page)
 Invoke-WebRequest -Uri $install_script_url -OutFile "C:/tabsetup/ScriptedInstaller.py"
 
 ## 4. Download python .msi
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-WebRequest -Uri "https://www.python.org/ftp/python/2.7.12/python-2.7.12.msi" -OutFile "C:/tabsetup/python-2.7.12.msi"
 
-## 5. download Tableau Server 2018.1 .exe
+## 5. Download Tableau Server 2018.1 .exe
 Invoke-WebRequest -Uri $install_script_url -Outfile "C:/tabsetup/tableau-server-installer.exe"
 
 ## COMMANDS
 
-## 1. install python (and add to path) - wait for install to proceed
+## 1. Install python (and add to path) - wait for install to finish
 Start-Process "c:/tabsetup/python-2.7.12.msi" -ArgumentList "/quiet /qn" -Wait
 $env:Path = "C:/Python27/"
 
-## 2. install yaml
+## 2. Install yaml module
 Set-Location -Path C:/Python27/Scripts
 .\pip install pyyaml
 Set-Location -Path C:/Python27/Scripts
 
-## 2.5 make tabinstall.txt
+## 2.5 Make tabinstall.txt
 New-Item c:/tabsetup/tabinstall.txt -ItemType file
 
-## 3. run installer script & accomodate for trial key
+## 3. Run installer script & accomodate for trial key
 cd C:/Python27/
 if ($license_key.ToLower() = "trial") {
     ./python C:/tabsetup/ScriptedInstaller.py install --installerLog C:/tabsetup/tabinstall.txt --enablePublicFwRule --secretsFile C:/tabsetup/secrets.json --registrationFile C:/tabsetup/registration.json --installDir C:/Tableau/ --trialLicense C:/tabsetup/tableau-server-installer.exe
@@ -83,9 +80,5 @@ if ($license_key.ToLower() = "trial") {
     ./python C:/tabsetup/ScriptedInstaller.py install --installerLog C:/tabsetup/tabinstall.txt --enablePublicFwRule --secretsFile C:/tabsetup/secrets.json --registrationFile C:/tabsetup/registration.json --installDir C:/Tableau/ --licenseKey $license_key C:/tabsetup/tableau-server-installer.exe
 }
 
-
-##  c:/tabsetup/installer-output.txt 2>&1
-
-## 4. cleanup secrets
-
+## 4. Clean up secrets
 del c:/tabsetup/secrets.json
